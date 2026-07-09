beac3a03572336aba393e9e506058dfbf1ef8a25
Astr092 <156329960+Astr092@users.noreply.github.com>
2026-07-09 19:07:49 -0400

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,7 @@
+  export const firebaseConfig = {
+
+    apiKey: "AIzaSyAJAN7RGhEFTQH3uhafZWeWyiMQ6JgZq4g",
+    authDomain: "random-outlier-chooser-or-smth.firebaseapp.com",
+    projectId: "  random-outlier-chooser-or-smth"
+
+};
---
+++ script.js
@@ -0,0 +1,197 @@
+import { firebaseConfig } from "./firebase.js";
+
+import {
+initializeApp
+} from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import {
+getFirestore,
+doc,
+setDoc,
+getDoc,
+collection,
+getDocs,
+updateDoc
+}
+from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+
+let playerID =
+localStorage.getItem("playerID");
+
+if(!playerID){
+    playerID =
+    crypto.randomUUID();
+
+    localStorage.setItem(
+        "playerID",
+        playerID
+    );
+}
+
+
+let roomCode;
+let playerName;
+
+
+async function joinGame(){
+
+    playerName =
+    document.getElementById("name").value;
+
+    roomCode =
+    document.getElementById("room").value;
+
+
+    await setDoc(
+        doc(
+            db,
+            "rooms",
+            roomCode,
+            "players",
+            playerID
+        ),
+        {
+            name: playerName,
+            role:null
+        }
+    );
+
+
+    document.getElementById("join").hidden=true;
+    document.getElementById("game").hidden=false;
+
+    document.getElementById(
+        "roomDisplay"
+    ).innerText=roomCode;
+
+
+    loadPlayers();
+
+}
+
+
+
+async function loadPlayers(){
+
+    let list =
+    document.getElementById("players");
+
+    list.innerHTML="";
+
+
+    let snapshot =
+    await getDocs(
+        collection(
+            db,
+            "rooms",
+            roomCode,
+            "players"
+        )
+    );
+
+
+    snapshot.forEach(p=>{
+
+        let li =
+        document.createElement("li");
+
+        li.innerText =
+        p.data().name;
+
+        list.appendChild(li);
+
+    });
+
+}
+
+
+
+async function startGame(){
+
+    let players =
+    await getDocs(
+        collection(
+            db,
+            "rooms",
+            roomCode,
+            "players"
+        )
+    );
+
+
+    let ids=[];
+
+    players.forEach(
+        p=>ids.push(p.id)
+    );
+
+
+    let chosen =
+    ids[
+        Math.floor(
+            Math.random()*ids.length
+        )
+    ];
+
+
+    for(let id of ids){
+
+        await updateDoc(
+            doc(
+                db,
+                "rooms",
+                roomCode,
+                "players",
+                id
+            ),
+            {
+                role:
+                id===chosen
+                ?
+                "outlier"
+                :
+                "normal"
+            }
+        );
+
+    }
+
+
+    alert("Game started!");
+
+}
+
+
+
+async function revealRole(){
+
+    let player =
+    await getDoc(
+        doc(
+            db,
+            "rooms",
+            roomCode,
+            "players",
+            playerID
+        )
+    );
+
+
+    document.getElementById(
+        "result"
+    ).innerText =
+    player.data().role==="outlier"
+    ?
+    "You are the OUTLIER!"
+    :
+    "You are NOT the outlier.";
+
+}
